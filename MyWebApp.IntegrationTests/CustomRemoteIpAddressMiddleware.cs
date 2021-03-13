4e9e878755e366e6d89b0400491dd1da32c85359
Changhui Xu <changhuixu@hotmail.com>
2021-03-12 23:30:33 -0600

    add integration tests

---
+++ MyWebApp.IntegrationTests/CustomRemoteIpAddressMiddleware.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace MyWebApp.IntegrationTests
+{
+    public class CustomRemoteIpStartupFilter : IStartupFilter
+    {
+        private readonly IPAddress _ipAddress;
+
+        public CustomRemoteIpStartupFilter(IPAddress ipAddress = null)
+        {
+            _ipAddress = ipAddress ?? IPAddress.Parse("127.0.0.1");
+        }
+
+        public Action<IApplicationBuilder> Configure(Action<IApplicationBuilder> next)
+        {
+            return app =>
+            {
+                app.UseCustomRemoteIpAddressMiddleware(c => c.IpAddress = _ipAddress);
+                next(app);
+            };
+        }
+    }
+
+    public static class CustomRemoteIpAddressMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomRemoteIpAddressMiddleware(this IApplicationBuilder app, Action<RemoteIpOptions> setupAction = null)
+        {
+            var options = new RemoteIpOptions
+            {
+                IpAddress = IPAddress.Parse("127.0.0.1")
+            };
+            setupAction?.Invoke(options);
+            return app.UseMiddleware<CustomRemoteIpAddressMiddleware>(options);
+        }
+    }
+
+    public class RemoteIpOptions
+    {
+        public IPAddress IpAddress { get; set; }
+    }
+
+    public class CustomRemoteIpAddressMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IPAddress _fakeIpAddress;
+
+        public CustomRemoteIpAddressMiddleware(RequestDelegate next, RemoteIpOptions fakeIpAddress)
+        {
+            _next = next;
+            _fakeIpAddress = fakeIpAddress.IpAddress;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            httpContext.Connection.RemoteIpAddress = _fakeIpAddress;
+            await _next(httpContext);
+        }
+    }
+}
---
+++ MyWebApp.IntegrationTests/IpRestrictionTests.cs
@@ -0,0 +1,87 @@
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyWebApp.IntegrationTests
+{
+    [TestClass]
+    public class IpRestrictionTests
+    {
+        [TestMethod]
+        public async Task HttpRequestWithAllowedIpAddressShouldReturn200()
+        {
+            var factory = new WebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.UseSetting("https_port", "5001");
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IStartupFilter>(new CustomRemoteIpStartupFilter(IPAddress.Parse("127.0.0.1")));
+                });
+            });
+            var client = factory.CreateClient();
+            var response = await client.GetAsync("values");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var json = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("[\"value1\",\"value2\"]", json);
+        }
+
+        [TestMethod]
+        public async Task HttpRequestWithForbiddenIpAddressShouldReturn403()
+        {
+            var factory = new WebApplicationFactory<Startup>().WithWebHostBuilder(builder =>
+            {
+                builder.UseSetting("https_port", "5001");
+                builder.ConfigureTestServices(services =>
+                {
+                    services.AddSingleton<IStartupFilter>(new CustomRemoteIpStartupFilter(IPAddress.Parse("127.168.1.32")));
+                });
+            });
+            var client = factory.CreateClient();
+            var response = await client.GetAsync("values");
+
+            Assert.AreEqual(HttpStatusCode.Forbidden, response.StatusCode);
+            Assert.AreEqual("application/problem+json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+        }
+
+        [Ignore("I haven't figured out how to test in this way.")]
+        [TestMethod]
+        public async Task HttpRequestWithLocalHostIpAddressShouldReturn200()
+        {
+            var factory = new WebApplicationFactory<Startup>()
+                .WithWebHostBuilder(builder => builder.UseSetting("https_port", "5001"));
+            var client = factory.CreateClient(new WebApplicationFactoryClientOptions
+            {
+                AllowAutoRedirect = true
+            });
+
+            //var context = await factory.Server.SendAsync((c) =>
+            //{
+            //    c.Connection.RemoteIpAddress = IPAddress.Parse("127.168.1.32");
+            //    c.Request.Method = HttpMethods.Get;
+            //    c.Request.Path = new PathString("/values");
+
+            //});
+            //var response = context.Response;
+
+            //Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            //Assert.AreEqual("application/json; charset=utf-8", response.ContentType);
+
+            //var json =  response.Body.ToString();
+
+            var response = await client.GetAsync("values");
+
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+            Assert.AreEqual("application/json; charset=utf-8", response.Content.Headers.ContentType?.ToString());
+
+            var json = await response.Content.ReadAsStringAsync();
+            Assert.AreEqual("[\"value1\",\"value2\"]", json);
+        }
+    }
+}
